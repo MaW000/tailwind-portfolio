48c2ce812ad1140b7e1efbacb2126abf88301358
matt wardlow <mwardlow0@gmail.com>
2022-12-15 23:40:53 -0500

    Style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { BsOctagon } from "react-icons/bs"
 import useScroll from "../hooks/useScroll";
+import './header.css';
 const Header = () => {
     const { scrollY, scrollDirection } = useScroll();
 
 	const [mobileNav, setMobileNav] = React.useState(false);
     return (
         
-        <header className={"h-24 fixed min-w-full"}>
-            <nav className={scrollY >= 250 ? scrollDirection === 'down' ? 'bg-black w-full h-full align-center  flex justify-between ' : 'hidden' : 'bg-black w-full h-full align-center  flex'}>
+        <header className={scrollY >= 250 ? scrollDirection === 'down' ? "h-24  min-w-full z-50 animate-bounce transition-all translate-y-100 duration-1000 " : ' translate-y-100 duration-1000 transition-all animate-bounce ease-up duration-1000 hidden' : 'h-24  min-w-full z-50'}>
+            <nav
+            className={scrollY >= 250 ? scrollDirection === 'down' ? 'bg-maroon dog  w-full h-full align-center  flex justify-between translate-y-100 duration-1000 ' : 'transition-all  ease-up duration-1000 hidden translate-y-100 duration-1000' : 'bg-maroon w-full h-full align-center  flex ease-up duration-700 translate-y-100 duration-1000'}>
                 <div className="flex flex-col flex-wrap items-center justify-between min-w-full px-12 text-5xl md:flex-row" >
-                    <a className='relative flex items-center mb-4 font-medium text-gray-900 md:mb-0' href={ '/'}>
+                    <a
+                        className='relative dog flex items-center mb-4 font-medium text-gray-900 md:mb-0 transition-all  ease-up duration-1000' href={ '/'}>
                         <BsOctagon size={45} color="#ffa500" />
-                        <span class="ml-3 text-3xl font-[Proxima Nova] text-lightOrange absolute right-[9px] top-1  ">M</span>
+                        <span class="ml-3 text-3xl font-[Proxima Nova] text-lightOrange absolute right-[9px] top-1">M</span>
                     </a>
-                    <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto">
+                    <nav data-aos="fade-up" class="flex flex-wrap items-center justify-center text-base md:ml-auto">
                         <a href={ '/'} class="mr-5 text-[#CCD6F1] before:content-['1.'] before:text-lightOrange hover:text-lightOrange"> About</a>
                         <a href={ '/'} class="mr-5 text-[#CCD6F1] before:content-['2.'] before:text-lightOrange hover:text-lightOrange"> Experience</a>
                         <a href={ '/'} class="mr-5 text-[#CCD6F1] before:content-['3.'] before:text-lightOrange hover:text-lightOrange"> Work</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        'up': 'cubic-bezier(0.645, 0.045, 0.355, 1)'
+      },
       colors: {
         lightOrange: '#F1B24A',
         darkOrange: '#ff8c00',
@@ -10,7 +13,8 @@ module.exports = {
         darkslate: '#495670',
         lightestnavy: '#233554',
         lightnavy: '#112240',
-        lightslate: '#a8b2d1'
+        lightslate: '#a8b2d1',
+        maroon: '#301008'
       }
     },
   },
